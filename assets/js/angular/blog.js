5005601eb1c5dc6a9933ef5953bbf5cb2ec95ec9
Jackie Swanson <jswanson@sbts.edu>
2016-03-25 01:31:46 -0400

    I think my project's done

--- assets/js/angular/blog.js
+++ assets/js/angular/blog.js
@@ -16,8 +16,8 @@ url: "https://www.googleapis.com/blogger/v3/blogs/2644023499243787092/posts?key=
 success: function(data){
  $.each(data.items, function(index, element) {
   //  $(".content p").html(element.content);
-    $('<h2 />', {html: element.title}).addClass("blogTitle").appendTo(".content");
-    $('<p />', { html: element.content }).addClass("blogPost").appendTo(".content");
+    $('<h2 />', {html: element.title}).addClass("blog-title").appendTo(".content");
+    $('<div />', { html: element.content }).addClass("blog-post").appendTo(".content");
  });
   //$(".content p").html(data.items[0].content);
 }
